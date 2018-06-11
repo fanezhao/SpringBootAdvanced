3682a4a3a29a1ee31be9c21c9bb00b0fae530408
fanezhao <fanezhao@gmail.com>
2018-06-11 18:12:20 +0800

    初始化 SpringBoot整合安全 项目Demo

---
+++ springbootwithsecurity/src/main/java/com/zfspace/security/springbootwithsecurity/SpringbootwithsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.zfspace.security.springbootwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwithsecurityApplication.class, args);
+    }
+}
---
+++ springbootwithsecurity/src/test/java/com/zfspace/security/springbootwithsecurity/SpringbootwithsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zfspace.security.springbootwithsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootwithsecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
